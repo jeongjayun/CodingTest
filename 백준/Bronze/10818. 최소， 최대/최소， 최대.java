869227102b6b7f72638ddd0ad7f0c1970fdb2281
jeongjayun <116062065+jeongjayun@users.noreply.github.com>
2023-12-14 22:01:06 +0900

    [Bronze III] Title: 최소, 최대, Time: 1808 ms, Memory: 334528 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // N개의 정수 입력
+
+		List<Integer> arr = new ArrayList<>();
+
+		int min = Integer.MAX_VALUE; // 최소값을 저장할 변수를 초기화
+		int max = Integer.MIN_VALUE; // 최대값을 저장할 변수를 초기화
+
+		for (int i = 0; i < N; i++) {
+			int num = sc.nextInt(); // N개의 정수를 num으로 입력받음
+			arr.add(num);
+
+			// 입력된 정수로 최소값 및 최대값 업데이트
+			min = Math.min(min, num);
+			max = Math.max(max, num);
+
+		}
+		System.out.println(min + " " + max);
+	}
+
+}
